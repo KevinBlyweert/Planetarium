c71ee5e04436d0057f7df9972f78c9fd65c33d7a
Neithan et Eldalote Coemgen <kevin.blyweert@gmail.com>
2024-05-16 18:45:22 +0200

    added close up view for four first planets

--- assets/js/index.js
+++ assets/js/index.js
@@ -48,44 +48,68 @@ let height = (canvas.height = window.innerHeight);
 // loop();
 
 const solarSytem = [];
+const closeUp = [];
 let systemSize = 0;
+let closeUpSystemSize = 0;
 
 async function createSystem() {
     fetch("./assets/js/utils/planetInfo.json")
         .then((res) => {
             if (!res.ok) { throw new Error("Not ok.") }
-            return res.json()
+            return res.json();
         })
         .then((data) => {
             systemSize = data[data.length - 1].distanceToSun;
+            closeUpSystemSize = data[4].distanceToSun
             data.forEach(item => {
-                const planet = new Ball(width / 2, ((item.distanceToSun * (height - 100)) / systemSize) + 50, 0, 0, "yellow", (item.radius * (height - 20)) / systemSize * ((item.name) == "Sun" ? 30 : 1000), item.name, "fill", item.speed);
-                // const planet = new Ball(width / 2, ((item.distanceToSun * height) / systemSize), 0, 0, "yellow", 20, item.name);
-                solarSytem.push(planet)
+                const planet = new Ball(width / 2, ((item.distanceToSun * (height - 100)) / systemSize), 0, 0, "yellow", (item.radius * (height - 100)) / systemSize * ((item.name) == "Sun" ? 50 : 1500), item.name, "fill", item.speed);
+                solarSytem.push(planet);
+                if (closeUp.length != 5) {
+                    const closePlanet = new Ball(height / 4, (((height / 4) * item.distanceToSun) / closeUpSystemSize), 0, 0, "white", (item.radius * (height / 4)) / closeUpSystemSize * ((item.name) == "Sun" ? 40 : 1000), item.name, "fill", item.speed)
+                    closeUp.push(closePlanet);
+                }
             })
+            animateSystem();
         })
 }
 
 createSystem()
 
 function animateSystem() {
-    ctx.fillStyle = "#1D1135";
-    ctx.fillRect(0, 0, width, height);
+    if (solarSytem.length != 0) {
+        ctx.fillStyle = "#000100";
+        ctx.fillRect(0, 0, width, height);
 
-    solarSytem.forEach((planet => {
-        if (planet.name !== "Sun") {
-            const course = new Ball(width / 2, 50, 0, 0, "#ccc", planet.distanceToSun, "", "stroke");
+        solarSytem.forEach((planet => {
+            const course = new Ball(width / 2, 50, 0, 0, "#8B9BC1", planet.distanceToSun, "", "stroke");
             course.draw(ctx);
             planet.planetCourseUpdate(course);
-        }
-        planet.draw(ctx);
-    }))
-    requestAnimationFrame(animateSystem)
+            planet.draw(ctx);
+        }));
+
+        // Four first planets close up
+        ctx.fillStyle = "#000100";
+        ctx.fillRect(0, height - (height / 2), (height / 2), (height / 2));
+        ctx.strokeStyle = "#8B9BC1";
+        ctx.strokeRect(0, height - (height / 2), (height / 2), (height / 2));
+        closeUp.forEach(closePlanet => {
+            const course = new Ball(height / 4, (height / 2) + (height / 4), 0, 0, "#8B9BC1", closePlanet.distanceToSun, "", "stroke");
+            course.draw(ctx);
+            closePlanet.planetCourseUpdate(course);
+            closePlanet.draw(ctx);
+        })
+        // for (let index = 0; index < 5; index++) {
+        //     const planet = closeUp[index];
+        //     const course = new Ball(height / 4, (height / 2) + (height /4), 0, 0, "#8B9BC1", planet.distanceToSun, "", "stroke");
+        //     course.draw(ctx);
+        //     // planet.planetCourseUpdate(course);
+        //     planet.draw(ctx);
+        // }
+    }
+    requestAnimationFrame(animateSystem);
 }
 
-animateSystem()
 window.addEventListener("resize", (e) => {
     width = (canvas.width = window.innerWidth);
     height = (canvas.height = window.innerHeight);
-    console.log(solarSytem);
 })
